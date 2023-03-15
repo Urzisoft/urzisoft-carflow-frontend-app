f57dce4b2ef3cb29d142fb412253b48588adb826
DragosBaci <dragos6171@gmail.com>
2023-03-15 21:02:19 +0200

    CFFE-2 : implementing intro page

--- src/Components/Pages/Welcome/Welcome.tsx
+++ src/Components/Pages/Welcome/Welcome.tsx
@@ -1,11 +1,23 @@
-import { WelcomeContainer } from "./Welcome.css";
+import { WelcomeContainer, WelcomeTitle, WelcomeButton, WelcomeText, BackgroundVideo} from "./Welcome.css";
+import carVideo from "./Assets/CarVideo.mp4";
 import { FC } from "react";
 
 export const Welcome: FC = () => {
+  return (
+    <WelcomeContainer>
+      <BackgroundVideo autoPlay muted loop>
+        <source src={carVideo} type="video/mp4" />
+      </BackgroundVideo>
+      <WelcomeTitle>CarFlow</WelcomeTitle>
+      <WelcomeText>
+        Provides you with the best possible vehicle management experience,
+        saving you time and hassle. No more keeping track of piles of paperwork
+        or manually recording maintenance appointments.
+      </WelcomeText>
 
-    return (
-        <WelcomeContainer>
-            Welcome Page
-        </WelcomeContainer>
-    );
-}
+      <WelcomeButton as="a" href="#">
+        Continue
+      </WelcomeButton>
+    </WelcomeContainer>
+  );
+};
