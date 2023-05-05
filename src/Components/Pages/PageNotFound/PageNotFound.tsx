e046b07d2c4574344fffd56fc039ab38734a515a
Gafencu Beniamin <benigafencu@yahoo.com>
2023-05-05 22:25:26 +0300

    Delete Token use

--- src/Components/Pages/PageNotFound/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -1,12 +1,10 @@
 import { FC} from "react";
-import useValidateUser from "../../../Hooks/useValidateUser";
 import { OverlayNotification } from "../../Common/OverlayNotification/OverlayNotification";
 import { useAuth } from "../../../Hooks/useAuth";
 
 export const PageNotFound: FC = () => {
 
     const { isLoggedIn } = useAuth();
-    const { token } = useValidateUser();
 
     if (!isLoggedIn) {
         return <OverlayNotification message={'Authentication required'} />;
