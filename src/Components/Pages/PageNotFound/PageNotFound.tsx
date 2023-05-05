e4f35622e10c0feb028247617310ec8f409bb638
Gafencu Beniamin <benigafencu@yahoo.com>
2023-05-05 22:17:31 +0300

    Delete useless imports

--- src/Components/Pages/PageNotFound/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC} from "react";
 import useValidateUser from "../../../Hooks/useValidateUser";
 import { OverlayNotification } from "../../Common/OverlayNotification/OverlayNotification";
 import { useAuth } from "../../../Hooks/useAuth";
