4d49680607b73558c7427a886b9912b5bc60597f
Gafencu Beniamin <benigafencu@yahoo.com>
2023-05-05 22:35:45 +0300

    Delete unwanted code

--- src/Components/Pages/PageNotFound/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,8 @@
-import { FC} from "react";
+import { FC } from "react";
 import { OverlayNotification } from "../../Common/OverlayNotification/OverlayNotification";
-import { useAuth } from "../../../Hooks/useAuth";
 
 export const PageNotFound: FC = () => {
 
-    const { isLoggedIn } = useAuth();
-
-    if (!isLoggedIn) {
-        return <OverlayNotification message={'Authentication required'} />;
-    }
     return (
         <>
             return <OverlayNotification message={'404: Page not found'} />;
