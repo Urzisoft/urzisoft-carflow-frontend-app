6fd0286eb6375c2b0c55fd85607318f60ed8dfe8
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-06 20:44:52 +0300

    Updates

--- src/Components/Pages/PageNotFound/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -2,10 +2,9 @@ import { FC } from "react";
 import { OverlayNotification } from "../../Common/OverlayNotification/OverlayNotification";
 
 export const PageNotFound: FC = () => {
-
     return (
         <>
-            return <OverlayNotification message={'404: Page not found'} />;
+            <OverlayNotification message={'404: Page not found'} />;
         </>
     )
 }
